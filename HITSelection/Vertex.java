8cf6d290c5376aaac9c64094881770147ad5af0c
Shuang Hao <87415201+Shuang-H@users.noreply.github.com>
2021-11-09 20:18:41 +0800

    Create Vertex.java

---
+++ HITSelection/Vertex.java
@@ -0,0 +1,13 @@
+public class Vertex {
+	public double weight;
+	public int label;
+	public String triple;
+	
+	
+	public Vertex(double weight, int label, String triple){
+		this.weight = weight;
+		this.label = label;
+		this.triple = triple;
+		
+	}
+}
